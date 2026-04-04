dac286bfb16a1a544dce5dc971139f62004ae383
crazy0codes <madhanpanja@gmail.com>
2026-04-04 16:45:32 +0000

    feat(records): add record routes and controllers

---
+++ src/controllers/recordController.js
@@ -0,0 +1,62 @@
+import {
+    createRecord,
+    getAllRecords,
+    updateRecord,
+    deleteRecord
+} from "../services/recordService"
+
+
+export async function createRecordController(req, res) {
+    try {
+        const userId = parseInt(req.user.id);
+        const record = await createRecord(userId, req.body)
+        return res.status(201).json({
+            record
+        })
+    } catch (error) {
+        return res.status(error.status).json({
+            error: error.message
+        })
+    }
+}
+
+export async function getAllRecordsController(req, res) {
+    try {
+        const records = await getAllRecords()
+        return res.status(200).json({
+            records
+        })
+    } catch (error) {
+        return res.status(error.status).json({
+            error: error.message
+        })
+    }
+}
+
+export async function updateRecordController(req, res) {
+    try {
+        const id     = parseInt(req.params.id)
+        const record = await updateRecord(id , req.body);
+        return res.status(200).json({
+            ...record
+        })
+    } catch (error) {
+        return res.status(error.status).json({
+            error: error.message
+        })
+    }
+}
+
+export async function deleteRecordController(req, res) {
+    try {
+        const id = parseInt(req.params.id);
+        const record = await deleteRecord(id);
+        return res.status(200).json({
+            record
+        })
+    } catch (error) {
+        return res.status(error.status).json({
+            error: error.message
+        })
+    }
+}
---
+++ src/routes/records.js
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import {
+    deleteRecordController,
+    createRecordController,
+    getAllRecordsController,
+    updateRecordController
+} from "../controllers/recordController"
+
+import { auth } from "../middlewares/auth"
+import { allow } from "../middlewares/rbac"
+
+const router = Router();
+
+router.get("/", auth, allow("VIEWER", "ANALYST", "ADMIN"), getAllRecordsController)
+router.post("/", auth, allow("ADMIN"), createRecordController)
+router.patch("/:id", auth, allow("ADMIN"), updateRecordController)
+router.delete("/:id", auth, allow("ADMIN"), deleteRecordController)
+
+export default router
